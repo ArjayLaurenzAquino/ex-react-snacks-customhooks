fb05ea885d3e275798d43aa77b0428e50b3c9d0e
Arjay Laurenz Aquino <aquinoarjay141@gmail.com>
2026-05-25 18:48:26 +0200

    first commit

---
+++ src/useSwitch.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+function useSwitch(initialValue = false) {
+    const [isOn, setIsOn] = useState(initialValue)
+    const toggle = () => setIsOn(!isOn)
+
+    return { isOn, toggle }
+
+}
+
+export default useSwitch
